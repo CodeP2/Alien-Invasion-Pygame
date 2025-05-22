913e16cab0cb662e478bbf52a39c334c14c1267e
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-05-22 15:24:14 +0200

    added .event. to line 19

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,7 +16,7 @@ class AlienInvasion:
         """Start main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.get():
+            for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
             
