5aa9c52c237b5253c27983c8f7351cbce4051d8c
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-05-26 14:36:51 +0200

    added helper function _check_events() to run_game()

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,10 +22,7 @@ class AlienInvasion:
     def run_game(self):
         """Start main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
             
             # Redraw the screen durning each pass through the loop.
             self.screen.fill(self.settings.bg_color)
@@ -34,6 +31,12 @@ class AlienInvasion:
             # Make the most recently drawn screen visible.
             pygame.display.flip()
             self.clock.tick(60)
+    
+    def _check_events(self):
+        """Responds to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 
 if __name__ == '__main__':
