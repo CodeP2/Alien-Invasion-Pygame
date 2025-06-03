3b6d706bdca2b5c10eb0c50004f4ac7f59fb4a63
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-06-03 14:07:37 +0200

    Added bullet settings and bullet class

---
+++ bullet.py
@@ -0,0 +1,10 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship."""
+
+    def __init__(self):
+        """Create a bullet object at the ship's current position."""
+        pass
