832803211f58dc07dead28ebb9b0de9fa171d401
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-06-05 15:24:26 +0200

    imported alien class and created an instance.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -19,6 +20,7 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
     
     def run_game(self):
         """Start main loop for the game."""
