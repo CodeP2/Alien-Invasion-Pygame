17b2ce657ef4fc608dcf89a7824df13f4bf02bc9
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-06-10 13:57:58 +0200

    Added constant movement to the right.

--- settings.py
+++ settings.py
@@ -20,6 +20,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
     
     def screen_settings(self, full_screen=False):
         """Setting controling the game screen."""
