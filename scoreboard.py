1f9acf1cb038d47ca84b8c357cc25b9e7cab4b82
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-07-12 13:00:27 +0200

    Added a file to store a Scoreboard class.

---
+++ scoreboard.py
@@ -0,0 +1,9 @@
+import pygame.font
+
+
+class Scoreboard:
+    """A class to report scoring information."""
+
+    def __init__(self, ai_game):
+        """Initialize scorekeeping attributes."""
+        pass
