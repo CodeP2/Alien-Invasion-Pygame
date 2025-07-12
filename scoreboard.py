16a86bec5fea6d1dd9762b7aa1d1fea9a3d0103e
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-07-12 13:03:50 +0200

    completed __init__() in scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -6,4 +6,14 @@ class Scoreboard:
 
     def __init__(self, ai_game):
         """Initialize scorekeeping attributes."""
-        pass
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+        self.settings = ai_game.settings
+        self.stats = ai_game.stats
+
+        # Font settings for scoring information.
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # prepare the initial score image.
+        self.prep_score()
