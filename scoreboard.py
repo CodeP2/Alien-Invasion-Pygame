a7ef88fe1c67f118eb1b33ebed541576746d6496
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2025-07-14 14:05:35 +0200

    draw high score to the screen.

--- scoreboard.py
+++ scoreboard.py
@@ -33,8 +33,17 @@ class Scoreboard:
     
     def prep_high_score(self):
         """Trun the high score into a rendered image."""
-        pass
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = f"{high_score:,}"
+        self.high_score_image = self.font.render(high_score_str, True,
+                                                 self.text_color, self.settings.bg_color)
+        
+        # Center the high score at the top of the screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
     
     def show_score(self):
         """Draw score to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
